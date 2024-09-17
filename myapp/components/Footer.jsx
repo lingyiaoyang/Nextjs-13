786abd0488a219f94ed73b5459ecd8bb806979a2
lingyiaoyang666 <146689086+lingyiaoyang666@users.noreply.github.com>
2024-09-17 10:13:36 +0800

    Update Footer.jsx

--- myapp/components/Footer.jsx
+++ myapp/components/Footer.jsx
@@ -1,9 +1,9 @@
-import React from 'react';
+import React from "react";
 
 const footer = () => {
   const currentYear = new Date().getFullYear();
 
-  return <div>copy right 2001 - {currentYear}</div>;
+  return <div>copy right 2001 testestest - {currentYear}</div>;
 };
 
 export default footer;
